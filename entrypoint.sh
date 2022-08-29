b57881b3b6b7c97726ed76d7152c5e27213da057
Todd Meder <todd.meder@rightbrainetworks.com>
2022-08-29 16:43:40 -0400

    Adding more debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,13 @@ then
     # Runs auto-semver and grabs outputs
     export regex='^\\s*current_version\\s*=\\s*\\K[^\\s]+'
     export RETURN_STATUS=`semver -n -D`
+    export STATIC_TEST="Static string"
     echo "Semver Return Status: ${RETURN_STATUS}"
-
+    echo $STATIC_TEST
     export SEMVER_NEW_VERSION=`grep -Po '${regex}' .bumpversion.cfg`
     export VERSION=`semver_get_version -d`
 
+    echo "::set-output name=STATIC_TEST::$STATIC_TEST"
     echo "::set-output name=RETURN_STATUS::$RETURN_STATUS"
     echo "::set-output name=SEMVER_NEW_VERSION::$SEMVER_NEW_VERSION"
     echo "::set-output name=VERSION::$VERSION"
