33f80891998422c9234aecd8f58e56a0b7e6f441
Todd Meder <todd.meder@rightbrainetworks.com>
2022-08-30 13:04:31 -0400

    Echo semver command

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ if [ "$1" = "set" ]
 then
     # Runs auto-semver and grabs outputs
     export regex='^\\s*current_version\\s*=\\s*\\K[^\\s]+'
-    echo $(semver -n)
+    echo "$(semver -n)"
     export RETURN_STATUS=`semver -n -D`
     echo "Semver Return Status: ${RETURN_STATUS}"
     export SEMVER_NEW_VERSION=`grep -Po '${regex}' .bumpversion.cfg`
