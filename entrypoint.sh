3a450224b060b22bd8702611e0b7bff5564f512b
Todd Meder <todd.meder@rightbrainetworks.com>
2022-08-30 11:42:12 -0400

    Debug logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 if [ "$1" = "set" ]
 then
     # Runs auto-semver and grabs outputs
+    echo "Working directory is $(pwd)"
+    echo "$(ls)"
     export regex='^\\s*current_version\\s*=\\s*\\K[^\\s]+'
     export RETURN_STATUS=`semver -n -D`
     echo "Semver Return Status: ${RETURN_STATUS}"
