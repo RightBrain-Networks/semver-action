8b47641df5857b79951131ef9d3def75671340ec
Todd Meder <todd.meder@rightbrainetworks.com>
2022-08-30 12:56:11 -0400

    Echo semver help

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,12 @@ then
     echo "Working directory is $(pwd)"
     echo "$(ls -al)"
     export regex='^\\s*current_version\\s*=\\s*\\K[^\\s]+'
-    echo "$(semver -n -D)"
+    echo "$(semver -h)"
     export RETURN_STATUS=`semver -n -D`
     echo "Semver Return Status: ${RETURN_STATUS}"
     export SEMVER_NEW_VERSION=`grep -Po '${regex}' .bumpversion.cfg`
     echo "Semver new version: ${SEMVER_NEW_VERSION}"
     export VERSION=`semver_get_version -d`
-    echo "::set-output name=STATIC_TEST::$STATIC_TEST"
     echo "::set-output name=RETURN_STATUS::$RETURN_STATUS"
     echo "::set-output name=SEMVER_NEW_VERSION::$SEMVER_NEW_VERSION"
     echo "::set-output name=VERSION::$VERSION"
