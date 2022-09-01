4a59098beecd9025a5b058ad6d7cf584b34c4601
Todd Meder <todd.meder@rightbrainetworks.com>
2022-09-01 16:10:58 -0400

    Adds check command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,35 @@
 #!/bin/sh
 set -e
 
+compare_versions() {
+  export regex="^([0-9]+.[0-9]+.[0-9]+)"
+
+      git fetch --all --tags
+      TAGS=( $(git tag) )
+      declare -a VERSIONS=()
+
+      for i in "${TAGS[@]}"
+      do
+        if [[ "$i"  =~ $regex ]];
+        then
+           VERSIONS+=($i)
+        fi
+      done
+
+      if [ -z "$VERSIONS" ]; then
+        echo "$1"
+      else
+        sorted=( $(sort -V <<<"${VERSIONS[*]}") )
+        VERSION=${sorted[-1]}
+
+        if dpkg --compare-versions $1 gt $VERSION ; then
+          echo "$1"
+        else
+          echo $VERSION
+        fi
+      fi
+}
+
 if [ "$1" = "set" ]
 then
     # Runs auto-semver and grabs outputs
@@ -12,35 +41,21 @@ then
     echo "::set-output name=RETURN_STATUS::$RETURN_STATUS"
     echo "::set-output name=SEMVER_NEW_VERSION::$SEMVER_NEW_VERSION"
     echo "::set-output name=VERSION::$VERSION"
-elif [ "$1" = "get" ]
+elif [ "$1" = "check" ]
 then
-    # Updates .bumpversion files to tagged version
-    export regex="^([0-9]+.[0-9]+.[0-9]+)"
-
-    git fetch --all --tags
-    TAGS=( $(git tag) )
-    declare -a VERSIONS=()
-
-    for i in "${TAGS[@]}"
-    do
-      if [[ "$i"  =~ $regex ]];
-      then
-         VERSIONS+=($i)
-      fi
-    done
-
-    if [ -z "$VERSIONS" ]; then
-      echo "No tags found that comply with version numbering specified by https://semver.org/"
+    # Get highest tagged version(if exists), and compares to version from params. Sets VERSION equal to whichever is higher.
+    if [ -z "$2" ]
+    then
+      echo "Requires a semver version number for comparison"
     else
-      sorted=( $(sort -V <<<"${VERSIONS[*]}") )
-      VERSION=${sorted[-1]}
-    # bumpversion minor --no-tag --new-version ${VERSION}
-      echo ::set-output name=VERSION::$VERSION
+      NEWEST_VERSION=compare_versions "$2"
+      echo "::set-output name=VERSION::$NEWEST_VERSION"
     fi
 elif [ "$1" = "update" ]
 then
     # Updates .bumpversion files to tagged version
     VERSION="$2"
+
     bumpversion minor --no-tag --new-version ${VERSION}
     echo ::set-output name=VERSION::$VERSION
 fi
