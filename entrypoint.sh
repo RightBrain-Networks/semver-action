1183700afc1c89a2b5b422981b81d74ebb323383
Todd Meder <todd.meder@rightbrainetworks.com>
2022-09-01 12:23:35 -0400

    Get returns current tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,9 +16,11 @@ elif [ "$1" = "get" ]
 then
     # Updates .bumpversion files to tagged version
     export regex="([0-9]+.[0-9]+.[0-9]+)"
-    VERSION=`grep -Po ${regex} ${{ github.ref }}`
+    git fetch --all --tags
+    VERSION=`git tag -l --sort=refname ${regex} | tail -1`
+    echo $VERSION
     # bumpversion minor --no-tag --new-version ${VERSION}
-    echo ::set-output name=VERSION::$VERSION
+    # echo ::set-output name=VERSION::$VERSION
 elif [ "$1" = "update" ]
 then
     # Updates .bumpversion files to tagged version
